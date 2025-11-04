ba82122c48c5d32c874f6c75ecf65d42891cdc7b
Phoenix Gosper <59917842+FeenoPH@users.noreply.github.com>
2025-11-04 23:58:37 +1100

    Version 3: Added Remove Functionality
    - Added functionality for remove operation
    - When SKU doesn't exist, or quantity exceeds that of stock, returns early
    - When SKU does exist, and the quantity is less than that of the total stock amount, it adjusts the quantity column as necessary
    - When SKU does exist, and the quantity is equal to that of the total stock amount, that item is removed from the table.

--- InventorySystem/src/main/java/edu/cbrc/oRemove.java
+++ InventorySystem/src/main/java/edu/cbrc/oRemove.java
@@ -1,5 +1,9 @@
 package edu.cbrc;
 
+import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class oRemove implements Operation {
 
     private final double operationPermission = 7;
@@ -23,6 +27,57 @@ public class oRemove implements Operation {
 
     @Override
     public int run() {
-        return 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the SKU of the item you want to remove: ");
+        String itemSKUInput = sc.nextLine();
+        int itemQuantityInput;
+        while (true) {
+            System.out.println("Please enter the quantity of the item you want to remove: ");
+            try {
+                itemQuantityInput = sc.nextInt();
+                if (itemQuantityInput < 1) {
+                    throw new InputMismatchException();
+                }
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid quantity!");
+                return 0;
+            }
+        }
+        String url = "jdbc:sqlite:inventory.db";
+        try (Connection conn = DriverManager.getConnection(url)) {
+            conn.setAutoCommit(false);
+            try (PreparedStatement ps = conn.prepareStatement("SELECT * FROM inventory WHERE itemSku = ?")) {
+                ps.setString(1, itemSKUInput);
+                ResultSet rs = ps.executeQuery();
+                if (!rs.next()) {
+                    System.out.println("SKU DOES NOT EXIST!");
+                    return 0;
+                }
+                if (rs.getInt("itemQuantity") < itemQuantityInput) {
+                    System.out.println("Invalid quantity!");
+                    return 0;
+                }
+                if (rs.getInt("itemQuantity") == itemQuantityInput) {
+                    try (PreparedStatement ps2 = conn.prepareStatement("DELETE FROM inventory WHERE itemSku = ?")) {
+                        ps2.setString(1, itemSKUInput);
+                        ps2.executeUpdate();
+                    }
+                }
+                else {
+                    try (PreparedStatement ps2 = conn.prepareStatement("UPDATE inventory SET itemQuantity = ? WHERE itemSku = ?")) {
+                        ps2.setInt(1, rs.getInt("itemQuantity")-itemQuantityInput);
+                        ps2.setString(2, itemSKUInput);
+                        ps2.executeUpdate();
+                    }
+                }
+                conn.commit();
+                System.out.println("Item successfully removed!!");
+                return 0;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error connecting to the database: " + ex.getMessage());
+            return 0;
+        }
     }
 }
